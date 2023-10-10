341f7ae2d13cfe455d6bff247fa6875928ae9aed
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-10-10 00:35:29 -0300

    feat: aula 01 Java RocketSeat

--- src/main/java/br/com/marllonrodrigues/todolist/user/UserModel.java
+++ src/main/java/br/com/marllonrodrigues/todolist/user/UserModel.java
@@ -2,7 +2,7 @@ package br.com.marllonrodrigues.todolist.user;
 
 public class UserModel {
     
-    private String username;
-    private String name;
-    private String password;
+    public String username;
+    public String name;
+    public String password;
 }
